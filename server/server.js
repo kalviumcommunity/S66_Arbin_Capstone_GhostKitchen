8af79b0d186cc102f5f8013bc4d7e6c37d607cb6
Arbin-17 <arbinkumar015@gmail.com>
2025-04-21 13:23:24 +0530

    Reviewed and implemented

--- server/server.js
+++ server/server.js
@@ -4,17 +4,13 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 // Routes
 app.use("/api/foods", foodRoutes);
 
-// Catch-all route
-// app.use("*", (req, res) => {
-//   res.status(404).json({ message: "Route not found" });
-// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
